8c96d3856dbe734024b015581e608f6627af9e93
Igor Volochay <44619012+IgorVolochay@users.noreply.github.com>
2023-07-13 14:41:13 +0300

    🧰 Создание инструмента взаимодействия с Excel

--- tools/excel_worker.py
+++ tools/excel_worker.py
@@ -3,7 +3,7 @@ import openpyxl
 def get_tikker(path):
     workbook = openpyxl.load_workbook(path)
     workbook._active_sheet_index = 1 # Выбрать номер листа с данными
-    sheet_obj = workbook.active()
+    sheet_obj = workbook.active
 
     all_ticker = set()
     for index in range(2, sheet_obj.max_row + 1):
